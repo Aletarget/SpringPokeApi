21d2a6c10b7b0bb1d54e8c35b6eb6de6ca30176a
Aletarget <alejomora1205@gmail.com>
2024-10-02 18:46:27 -0500

    Creacion del dto para los datos espeficos a mostar

---
+++ pokeapi-springboot/src/main/java/com/pokeapi/pokeapi_springboot/Models/Pokemon.java
@@ -0,0 +1,12 @@
+package com.pokeapi.pokeapi_springboot.Models;
+
+import lombok.Data;
+
+@Data
+public class Pokemon {
+    private String name;
+    private int id;
+    private int height;
+    private int weight;
+    
+}
