4a55d9fb98e21f0c8ee24d46e7238326b1885d99
Aletarget <alejomora1205@gmail.com>
2024-10-01 20:05:19 -0500

    First Commit

---
+++ spring-poke-api/src/test/java/com/pokeapi/springboot/app/spring_poke_api/SpringPokeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pokeapi.springboot.app.spring_poke_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPokeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
